1d797adaae0d158a40b557475a40a3977b64d37d
creeperboom5019 <121186344+creeperboom5019@users.noreply.github.com>
2023-01-23 21:48:25 +0400

    Add files via upload

---
+++ kjhghikj.py
@@ -0,0 +1,81 @@
+
+result=0
+print("                                                                                                                                                             ")
+print("TABLE 1")
+print("1	3	5	7")
+print("9	11	13	15")
+print("17	19	21	23")
+print("25	27	29	31")
+print("                                                                                                                                                             ")
+anwser=input("Type 'Yes' if here is your birthday date and 'No' if no : ")
+if anwser=="Yes" :
+ result+=1
+if anwser=="yes" :
+ result+=1
+ 
+print("                                                                                                                                                             ")
+
+print("TABLE 2")
+print("2	3	6	7")
+print("10	11	14	15")
+print("18	19	22	23")
+print("26	27	30	31")
+print("                                                                                                                                                             ")
+anwser=input("Type 'Yes' if here is your birthday date and 'No' if no : ")
+if anwser=="Yes" :
+ result+=2
+if anwser=="yes" :
+ result+=2
+ 
+print("                                                                                                                                                             ")
+
+print("TABLE 3")
+print("4	5	6	7")
+print("12	13	14	15")
+print("20	21	22	23")
+print("28	29	30	31")
+print("                                                                                                                                                             ")
+anwser=input("Type 'Yes' if here is your birthday date and 'No' if no:  ")
+if anwser=="Yes" :
+ result+=4
+if anwser=="yes" :
+ result+=4
+
+ 
+print("                                                                                                                                                             ")
+
+
+print("TABLE 4")
+print("8	9	10	11")
+print("12	13	14	15")
+print("24	25	26	27")
+print("28	29	30	31")
+print("                                                                                                                                                             ")
+anwser=input("Type 'Yes' if here is your birthday date and 'No' if no : ")
+if anwser=="Yes" :
+ result+=8
+if anwser=="yes" :
+ result+=8
+
+print("                                                                                                                                                             ")
+
+
+print("TABLE 5")
+print("16	17	18	19")
+print("20	21	22	23")
+print("24	25	26	27")
+print("28	29	30	31")
+print("                                                                                                                                                             ")
+anwser=input("Type 'Yes' if here is your birthday date and 'No' if no : ")
+if anwser=="Yes" :
+ result+=16
+if anwser=="yes" :
+ result+=16
+
+
+print("                                                                                                                                                             ")
+print(f"your birthday date is {result} ")
+
+
+
+#be afraid of my apps maybe there is virus  ;)
